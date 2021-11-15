8e4efc2fca5fc0e54b57cefaab252301e2d1bd37
George Woltman <woltman@alum.mit.edu>
2021-11-14 21:47:21 -0800

    Added files for p95v307b9.

--- commonc.h
+++ commonc.h
@@ -12,7 +12,7 @@ extern "C" {
 /* Constants */
 
 #define VERSION		"30.7"
-#define BUILD_NUM	"8"
+#define BUILD_NUM	"9"
 /* The list of assigned OS ports follows: */
 /* Win9x (prime95) #1 */
 /* Linux (mprime)  #2 */
